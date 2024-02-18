cd4ed78e8b3407e55b77a6720084824279fa162b
John Doe <shakilansary9@gmail.com>
2024-02-18 23:04:08 +0530

    Post Added

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,26 @@ async function fetchBlogs() {
       revalidate: 10,
     },
   });
-  // const data = await res.json();
-  // return data.posts;
+  const data = await res.json();
+  return data.posts;
 }
 
 export default async function Home() {
   const posts = await fetchBlogs();
   console.log(posts);
-  return <main>hello next</main>;
+  return (
+    <main>
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 dark:text-slate-300">
+        {/* Blogs */}
+        <div className="grid grid-cols-4 gap-5 w-full">
+          {posts?.map((post: any, index: number) => (
+            <div key={index} className="p-4 border border-slate-600 rounded-xl">
+              <h2>{post.title}</h2>
+              <p>{post.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
